fb18ad4ed0a9e586361c90f3ea3b38f6b9122352
“Justin <dotjus@gmail.com>
2020-11-16 07:28:40 +1000

    Installed rolify gem. Blacked off create, update and delete functionality to listings items if user is not signed in. Updated front end styling so that links are more visible.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -14,18 +14,23 @@ class ListingsController < ApplicationController
 
   # GET /listings/new
   def new
-    user = current_user
-    @user = user.id
+    check_signed_in
+    @user = current_user
     @listing = Listing.new
+
   end
 
   # GET /listings/1/edit
   def edit
+    check_signed_in
+    @user = current_user
   end
 
   # POST /listings
   # POST /listings.json
   def create
+    check_signed_in
+    @user = current_user
     @listing = Listing.new(listing_params)
 
     respond_to do |format|
@@ -42,6 +47,7 @@ class ListingsController < ApplicationController
   # PATCH/PUT /listings/1
   # PATCH/PUT /listings/1.json
   def update
+    @user = current_user
     respond_to do |format|
       if @listing.update(listing_params)
         format.html { redirect_to @listing, notice: 'Listing was successfully updated.' }
@@ -56,6 +62,8 @@ class ListingsController < ApplicationController
   # DELETE /listings/1
   # DELETE /listings/1.json
   def destroy
+    check_signed_in
+
     @listing.destroy
     respond_to do |format|
       format.html { redirect_to listings_url, notice: 'Listing was successfully destroyed.' }
@@ -72,5 +80,12 @@ class ListingsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def listing_params
       params.require(:listing).permit(:name, :description, :duration, :cost, :user_id, :street_number, :street_name, :city, :postcode, :state)
+    end
+
+    def check_signed_in
+      if !user_signed_in?
+        flash[:alert] = "Please sign up to create and edit your own listings."
+        redirect_to new_user_registration_path
+      end
     end
 end
