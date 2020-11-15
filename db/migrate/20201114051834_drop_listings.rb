61c89c626ad76ae8ee3638934304e30cb5bfd158
“Justin <dotjus@gmail.com>
2020-11-16 08:32:34 +1000

    Front end update. Made colours more visible. Removed unwanted links fromlisting show page when users are not signed in.

--- db/migrate/20201114051834_drop_listings.rb
+++
@@ -1,5 +0,0 @@
-class DropListings < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :listings
-  end
-end
--- db/migrate/20201114052344_drop_postcodes.rb
+++
@@ -1,5 +0,0 @@
-class DropPostcodes < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :postcodes
-  end
-end
