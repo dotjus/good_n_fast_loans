061dc48c0db47a26af8b742db6fbd5e101e10d22
“Justin <dotjus@gmail.com>
2020-11-16 11:53:47 +1000

    Added my_hires & my_listings pages. Completed my_listings to show the listings of items they listed. git add .

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def my_listings
+    @listings = Listing.all
+    @user = current_user
+  end
+
+  def my_hires
+    @listings = Listing.all
+    @user = current_user
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  get '/pages/my_listings', to: 'pages#my_listings', as: 'my_listings'
+  get '/pages/my_hires', to: 'pages#my_hires', as: 'my_hires'
   root to: "pages#home"
 
 end
