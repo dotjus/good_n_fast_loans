461d2d8949d51d7d16f722ce9f9dd8480e42fc7b
“Justin <dotjus@gmail.com>
2020-11-19 19:28:02 +1000

    Added server side validation for the creation of a new listing.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,4 +4,7 @@ class Listing < ApplicationRecord
   has_many :loans
   has_many :users, through: :loans
   has_one_attached :image
+
+  validates :name, :description, :duration, :cost, :street_number, :street_name, :city, :postcode, :state, presence: true
+
 end
